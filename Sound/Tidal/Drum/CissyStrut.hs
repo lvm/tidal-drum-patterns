c90d1ec584db40754f844af2368a2f9296537aa3
Mauro <lavaramano@gmail.com>
2016-10-11 14:43:21 -0300

    x -> c

--- Sound/Tidal/Drum/CissyStrut.hs
+++ Sound/Tidal/Drum/CissyStrut.hs
@@ -12,11 +12,11 @@ sn = "[c c c ~][c ~ c c][~ c c ~][c ~ c ~]" :: Pattern String
 ch = "[c c c ~][c ~ c c][~ c c ~][c ~ c ~]" :: Pattern String
 
 bd' = [
-  "[x ~ ~ x][~ x ~ ~][~ x ~ x][x ~ x ~]" :: Pattern String,
-  "[x ~ ~ x][~ ~ ~ x][~ x ~ x][x ~ x ~]" :: Pattern String
-]
+  "[c ~ ~ c][~ c ~ ~][~ c ~ c][c ~ c ~]" :: Pattern String,
+  "[c ~ ~ c][~ ~ ~ c][~ c ~ c][c ~ c ~]" :: Pattern String
+  ]
 sn' = [
-  "[~ ~ ~ ~][x ~ ~ x][~ x x ~][~ ~ ~ ~]" :: Pattern String,
-  "[~ ~ x ~][~ x x ~][x x ~ ~][~ ~ ~ ~]" :: Pattern String
-]
-oh = "[~ ~ ~ ~][~ ~ ~ ~][~ ~ ~ ~][x ~ x ~]" :: Pattern String
+  "[~ ~ ~ ~][c ~ ~ c][~ c c ~][~ ~ ~ ~]" :: Pattern String,
+  "[~ ~ c ~][~ c c ~][c c ~ ~][~ ~ ~ ~]" :: Pattern String
+  ]
+oh = "[~ ~ ~ ~][~ ~ ~ ~][~ ~ ~ ~][c ~ c ~]" :: Pattern String
--- Sound/Tidal/Drum/StrBtsDcGogo100.hs
+++ Sound/Tidal/Drum/StrBtsDcGogo100.hs
@@ -6,7 +6,7 @@ import Sound.Tidal.Context
 
 bps = (101/120)
 
-bd = "[x ~ ~ x][~ ~ x ~][~ ~ x ~][~ ~ ~ ~]" :: Pattern String
-sn = "[~ x ~ ~][x ~ ~ x][~ ~ ~ ~][x ~ ~ ~]" :: Pattern String
-ch = "[~ x x ~][x x ~ x][~ ~ ~ ~][~ x x ~]" :: Pattern String
-oh = "[~ ~ ~ ~][~ ~ ~ ~][x ~ ~ ~][~ ~ ~ ~]" :: Pattern String
+bd = "[c ~ ~ c][~ ~ c ~][~ ~ c ~][~ ~ ~ ~]" :: Pattern String
+sn = "[~ c ~ ~][c ~ ~ c][~ ~ ~ ~][c ~ ~ ~]" :: Pattern String
+ch = "[~ c c ~][c c ~ c][~ ~ ~ ~][~ c c ~]" :: Pattern String
+oh = "[~ ~ ~ ~][~ ~ ~ ~][c ~ ~ ~][~ ~ ~ ~]" :: Pattern String
