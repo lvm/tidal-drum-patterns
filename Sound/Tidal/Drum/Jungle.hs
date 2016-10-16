61b4db1f904e6a60585d84404f7572cd14366a5a
Mauro <mauro@sdf.org>
2016-10-15 23:06:33 -0300

    ffs

--- Sound/Tidal/Drum/Jungle.hs
+++ Sound/Tidal/Drum/Jungle.hs
@@ -16,6 +16,6 @@ sn = [
   "[~ 0 ~ ~] [~ 0 ~ 0]" :: Pattern Int,
   "[~ ~ 0 ~] [~ 0 [~ 0]/2 [~ 0]/2]" :: Pattern Int
   ]
-0 = "[0 0 0 0] [0 0 0 0]" :: Pattern Int
-0 = "[~ ~ ~ 0] [~ ~ 0 ~]" :: Pattern Int
+ch = "[0 0 0 0] [0 0 0 0]" :: Pattern Int
+oh = "[~ ~ ~ 0] [~ ~ 0 ~]" :: Pattern Int
 
