feedc86eef9b4324213ffbec67a2008f53691732
Mauro <lvm@users.noreply.github.com>
2020-07-11 07:22:59 -0300

    Fix Irregular1b

--- Sound/Tidal/Drum/Irregular1b.hs
+++ Sound/Tidal/Drum/Irregular1b.hs
@@ -4,6 +4,6 @@ module Sound.Tidal.Drum.Irregular1b where
 
 import Sound.Tidal.Context
 
-bd = "[t ~ t ~] [~ ~ ~ t [~ ~ t ~] [~ ~ ~ ~]" :: Pattern Bool
+bd = "[t ~ t ~] [~ ~ ~ t] [~ ~ t ~] [~ ~ ~ ~]" :: Pattern Bool
 sn = "[~ ~ ~ ~] [t ~ ~ ~] [~ ~ ~ ~] [t ~ ~ t]" :: Pattern Bool
 
